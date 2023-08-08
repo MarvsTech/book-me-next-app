4ea80267837f34980d0377ad6bbd1f35411fef21
MarvsTech <ramos.marvin@seedtech.ph>
2023-08-08 13:40:55 +0800

    email integration

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Mail\NotificationMail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +18,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/api/send-mail', function () {
+    $mailData = [
+        'client_name' => 'Marvin Ramos',
+        'title' => 'Account Created Successfully',
+        'email' => 'sample@sample.com',
+        'username' => 'SampleAccount',
+        'date' => 'July 15, 2023',
+        'message' => 'Your account has been successfully created. You can now start using our services!.',
+    ];
+    Mail::to('ramos.marvin@seedtech.ph')->send(new NotificationMail($mailData));
+    return 'Notification email sent successfully!';
 });
