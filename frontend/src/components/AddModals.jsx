55071304eafe5336103c418030c10169c9b90830
LuzIreneSeedTech <139109623+LuzIreneSeedTech@users.noreply.github.com>
2023-08-14 13:57:50 +0800

    figma to react

---
+++ frontend/src/components/AddModals.jsx
@@ -0,0 +1,124 @@
+import React from 'react'
+import {Modal, Form, Button, Container, Row, Col} from 'react-bootstrap'
+import { useState } from 'react';
+import woman from '../images/woman.svg'
+import { useLocation } from 'react-router-dom';
+
+const AddModals = ({showAdd, onCloseAdd}) => {
+
+    const location = useLocation();
+
+    const [username, setUsername] = useState(""); 
+    const [password, setPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [contact, setContact] = useState("");
+    const [email, setEmail] = useState("");
+
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        setSelectedFile(file);
+    };
+
+   const onSubmit = e => {
+    e.preventDefault();
+
+    // navigate('/settings');
+
+    setUsername('');
+    setPassword('');
+    setFirstName('');
+    setLastName('');
+    setContact('');
+    setEmail('');    
+   }
+
+  return (
+    <Container>
+        {/* <div className='add/edit'></div> */}
+        <Modal show={showAdd} onHide={onCloseAdd}>
+            <Modal.Header closeButton className='add-modal-header'>
+                {
+                    (location.pathname === '/doctor') ?
+                        <h1>Add Doctor</h1>
+                    :
+                        null
+                }
+            </Modal.Header>
+            
+            <Modal.Body>
+                <Form className='modal-form-container' onSubmit={e => onSubmit(e)}>
+                    <div className='change-profile'>
+                        <div style={{ display: 'inline-block', cursor: 'pointer' }} className='change-profile-img'>
+                            <img src={woman} alt="Preview" width="40" height="40" onClick={() => document.getElementById('fileInput').click()} />
+                        </div>
+                        <input id="fileInput" type="file" style={{ display: 'none' }} accept="image/*" onChange={handleFileChange} />
+                    </div>
+
+                    <div className='modal-form-content'>
+                        <Container>
+                            <Row>
+                                <Col className='left-col'>
+                                    <Form.Group className='form-group'>
+                                        <Form.Label>Username</Form.Label>
+                                        {/* make conditional here for updating and adding */}
+                                        <Form.Control type='text' value={username} placeholder='Enter Username' onChange={e => setUsername(e.target.value)} />
+                                    </Form.Group>
+
+                                    <Form.Group className='form-group'>
+                                        <Form.Label>First Name</Form.Label>
+                                        <Form.Control type='text' value={firstName} placeholder='Enter First Name' onChange={e => setFirstName(e.target.value)} />
+                                    </Form.Group>
+
+                                    <Form.Group className='form-group'>
+                                        <Form.Label>Contact</Form.Label>
+                                        <Form.Control type='text' value={contact} placeholder='Enter Contact' onChange={e => setContact(e.target.value)} />
+                                    </Form.Group>
+                                </Col>
+                                
+                                <Col className='right-col'>
+                                    <Form.Group className='form-group'>
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control type='password' value={password} placeholder='Enter Password' onChange={e => setPassword(e.target.value)} />
+                                    </Form.Group>
+
+                                    <Form.Group className='form-group'>
+                                        <Form.Label>Last Name</Form.Label>
+                                        <Form.Control type='text' value={lastName} placeholder='Enter First Name' onChange={e => setLastName(e.target.value)} />
+                                    </Form.Group>
+
+                                    <Form.Group className='form-group'>
+                                        <Form.Label>Email</Form.Label>
+                                        <Form.Control type='email' value={email} placeholder='Enter Contact' onChange={e => setEmail(e.target.value)} />
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                            
+                        </Container>
+                        
+                    </div>
+                    
+                    <div className='horizontal-line'></div>
+                    
+                    <div className='btn-container'>
+                        <Button type='submit' onSubmit={onCloseAdd}>Update Profile</Button>
+                    </div>
+                    
+                </Form>
+            </Modal.Body>
+        </Modal>
+
+        {/* <Modal show={show} onHide={onClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+      </Modal> */}
+    </Container>
+  )
+}
+
+export default AddModals
--- frontend/src/pages/UserLog.jsx
+++ frontend/src/pages/UserLog.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import Tables from '../components/Tables'
 
 const UserLog = () => {
+
+  const tableHead = [
+    {tableHead : 'name'},
+    {tableHead : 'role'},
+    {tableHead : 'ip'},
+    {tableHead : 'date_logged_in'},
+  ]
+
+  const tableBody = [
+    {
+      name : 'Jane Doe',
+      role : 'Doctor',
+      ip : 'hmmmmmmm',
+      date_logged_in : 'August 15, 2023',
+    },
+    {
+      name : 'Jane Doe',
+      role : 'Doctor',
+      ip : 'hmmmmmmm',
+      date_logged_in : 'August 15, 2023',
+    },
+  ]
+
   return (
-    <div>UserLog</div>
+    <div className='table-content-wrapper'>
+        <div className='table-content-header'></div>
+        <Tables columns={tableHead} data={tableBody} />
+    </div>
   )
 }
 
