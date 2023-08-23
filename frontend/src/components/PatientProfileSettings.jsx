ba25bf2f21a7fb44cce1ca572fffe30bd7b4c7ac
MarvsTech <139109514+MarvsTech@users.noreply.github.com>
2023-08-23 09:36:51 +0800

    fixing modal functionality

--- frontend/src/components/PatientProfileSettings.jsx
+++ frontend/src/components/PatientProfileSettings.jsx
@@ -14,87 +14,74 @@ const PatientProfileSettings = ({show, handleCloseModal, data}) => {
     const [contact_number, setContactNumber] = useState("");
     const [address, setAddress] = useState("");
 
-    // useEffect(() => {
-
-    //     if (data) {
-    //         setUsername(data.username);
-    //         setPassword(data.password);
-    //         setFirstName(data.firstName);
-    //         setLastName(data.lastName);
-    //         setContact(data.contact);
-    //         setEmail(data.email);
-    //     }
-
-    // }, [])
-
     const onSubmit = e => {
       e.preventDefault();
     }
-    // useState, useEffect, and onSubmit function for user data end
 
-  return (
-    <Modal className='edit-profile-modal' show={show} onHide={handleCloseModal}>
-        <Modal.Header closeButton></Modal.Header>
-        <Modal.Body className='booking-body'>
-            <Container>
-                <Form onSubmit={e => onSubmit(e)}>
-                    <Container>
-                        <Row>
-                            <Col md={5}>
-                                <FloatingLabel controlId="floatingInputFirstname" label="Firstname" className="mb-3">
-                                    <Form.Control type="text" value={firstname} onChange={e => setFirstname(e.target.value)} placeholder=""/>
-                                </FloatingLabel>
-                            </Col>
-                            <Col md={5}>
-                                <FloatingLabel controlId="floatingInputLastname" label="Lastname" className="mb-3">
-                                    <Form.Control type="text" value={lastname} onChange={e => setLastname(e.target.value)} placeholder=""/>
-                                </FloatingLabel>
-                            </Col>
-                            <Col md={2}>
-                                <FloatingLabel controlId="floatingInputMI" label="MI" className="mb-3">
-                                    <Form.Control type="text" value={middlename} onChange={e => setMI(e.target.value)} placeholder=""/>
-                                </FloatingLabel>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col md={6}>
-                                <FloatingLabel controlId="floatingInputSpecialization" label="Specialization" className="mb-3">
-                                    <Form.Control type="text" value={specialization} onChange={e => setSpecialization(e.target.value)} placeholder=""/>
-                                </FloatingLabel>
-                            </Col>
-                            <Col md={6}>
-                                <FloatingLabel controlId="floatingInputRoomNumber" label="Room Number" className="mb-3">
-                                    <Form.Control type="text" value={room_number} onChange={e => setRoomNumber(e.target.value)} placeholder=""/>
-                                </FloatingLabel>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col md={6}>
-                                <FloatingLabel controlId="floatingInputContactNumber" label="Contact Number" className="mb-3">
-                                    <Form.Control type="text" value={contact_number} onChange={e => setContactNumber(e.target.value)} placeholder=""/>
-                                </FloatingLabel>
-                            </Col>
-                            <Col md={6}>
-                                <FloatingLabel controlId="floatingInputEmail" label="Email Address" className="mb-3">
-                                    <Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder=""/>
-                                </FloatingLabel>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <FloatingLabel controlId="floatingTextareaAddress" label="Address" className="mb-3">
-                                    <Form.Control as="textarea" value={address} onChange={e => setAddress(e.target.value)} placeholder="" style={{ height: '100px', width: '100%' }}/>
-                                </FloatingLabel>
-                            </Col>
-                        </Row>
-                    </Container>
+    return (
+        <Modal className='edit-profile-modal' show={show} onHide={handleCloseModal}>
+            <Modal.Header closeButton></Modal.Header>
+            <Modal.Body className='booking-body'>
+                <Container>
+                    <Form onSubmit={e => onSubmit(e)}>
+                        <Container>
+                            <Row>
+                                <Col md={5}>
+                                    <FloatingLabel controlId="floatingInputFirstname" label="Firstname" className="mb-3">
+                                        <Form.Control type="text" value={firstname} onChange={e => setFirstname(e.target.value)} placeholder=""/>
+                                    </FloatingLabel>
+                                </Col>
+                                <Col md={5}>
+                                    <FloatingLabel controlId="floatingInputLastname" label="Lastname" className="mb-3">
+                                        <Form.Control type="text" value={lastname} onChange={e => setLastname(e.target.value)} placeholder=""/>
+                                    </FloatingLabel>
+                                </Col>
+                                <Col md={2}>
+                                    <FloatingLabel controlId="floatingInputMI" label="MI" className="mb-3">
+                                        <Form.Control type="text" value={middlename} onChange={e => setMI(e.target.value)} placeholder=""/>
+                                    </FloatingLabel>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col md={6}>
+                                    <FloatingLabel controlId="floatingInputSpecialization" label="Specialization" className="mb-3">
+                                        <Form.Control type="text" value={specialization} onChange={e => setSpecialization(e.target.value)} placeholder=""/>
+                                    </FloatingLabel>
+                                </Col>
+                                <Col md={6}>
+                                    <FloatingLabel controlId="floatingInputRoomNumber" label="Room Number" className="mb-3">
+                                        <Form.Control type="text" value={room_number} onChange={e => setRoomNumber(e.target.value)} placeholder=""/>
+                                    </FloatingLabel>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col md={6}>
+                                    <FloatingLabel controlId="floatingInputContactNumber" label="Contact Number" className="mb-3">
+                                        <Form.Control type="text" value={contact_number} onChange={e => setContactNumber(e.target.value)} placeholder=""/>
+                                    </FloatingLabel>
+                                </Col>
+                                <Col md={6}>
+                                    <FloatingLabel controlId="floatingInputEmail" label="Email Address" className="mb-3">
+                                        <Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder=""/>
+                                    </FloatingLabel>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <FloatingLabel controlId="floatingTextareaAddress" label="Address" className="mb-3">
+                                        <Form.Control as="textarea" value={address} onChange={e => setAddress(e.target.value)} placeholder="" style={{ height: '100px', width: '100%' }}/>
+                                    </FloatingLabel>
+                                </Col>
+                            </Row>
+                        </Container>
 
-                    <Button type='submit' onClick={handleCloseModal}>Update Profile</Button>
-                </Form>
-            </Container>
-        </Modal.Body>
-    </Modal>
-  )
+                        <Button type='submit' onClick={handleCloseModal}>Update Profile</Button>
+                        Patient
+                    </Form>
+                </Container>
+            </Modal.Body>
+        </Modal>
+    )
 }
 
 export default PatientProfileSettings
