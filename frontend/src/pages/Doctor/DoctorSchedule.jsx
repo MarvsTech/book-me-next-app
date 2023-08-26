44937bbf6210ae4c132256bd22229f38a5507252
MarvsTech <139109514+MarvsTech@users.noreply.github.com>
2023-08-27 07:34:24 +0800

    initial commit
    - adding page navigation for our doctor appointment
    - adding page navigation for our doctor schedule

--- frontend/src/pages/Doctor/DoctorSchedule.jsx
+++ frontend/src/pages/Doctor/DoctorSchedule.jsx
@@ -54,7 +54,7 @@ const DoctorSchedule = () => {
     <>
       <DashboardHeader name={ currentUser.name }/>
 
-      <PatientTable dataRow={appointments} handleShowModal={handleShowModal}/>
+      <PatientTable dataRow={appointments} handleShowModal={handleShowModal} itemPerPage={9}/>
 
       <ViewPatientModal show={showModal} onClose={handleCloseModal} dataRow={selectedRow}/>
     </>
