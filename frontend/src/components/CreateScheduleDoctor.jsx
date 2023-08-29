8cdf38be1cf296d0ebd10606b84eacd256bb822c
LuzIreneSeedTech <139109623+LuzIreneSeedTech@users.noreply.github.com>
2023-08-29 10:03:15 +0800

    fixed the doctor create schedule modal

--- frontend/src/components/CreateScheduleDoctor.jsx
+++ frontend/src/components/CreateScheduleDoctor.jsx
@@ -9,35 +9,66 @@ const CreateScheduleDoctor = ({show, handleCloseModal}) => {
     }
 
   return (
-    <Modal className='create-sched-modal' show={show} onHide={handleCloseModal}>
-        <Modal.Header closeButton></Modal.Header>
-        <Modal.Body className='booking-body create-sched-body'>
-            <Container>
-                <Form onSubmit={e => onSubmit(e)}>
-                    <Container>
-                        <Row>
-                            <Form.Label>Booking Date:</Form.Label>
-                            <Form.Control
-                                type="date"
-                                placeholder='Choose date'
-                            />
-                        </Row>
-                        <Row>
-                            <Form.Label>Booking Time:</Form.Label>
-                            <Form.Select>
-                                <option>Choose Time</option>
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                            </Form.Select>
-                        </Row>
-                    </Container>
-
-                    <Button type='submit' onClick={handleCloseModal}>Create Schedule</Button>
-                </Form>
-            </Container>
-        </Modal.Body>
-    </Modal>
+    <div >
+        <Modal className='create-sched-modal' show={show} onHide={handleCloseModal}>
+            <Modal.Header className='create-sched-header' closeButton>
+                <div>Create Schedule</div>
+            </Modal.Header>
+            <Modal.Body className='booking-body create-sched-body'>
+                <Container fluid>
+                    <Form onSubmit={e => onSubmit(e)}>
+                        <Container fluid>
+                            <Row>
+                                <Form.Label>Title</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder='enter schedule title'
+                                />
+                            </Row>
+                            <Row>
+                                <Form.Label>Description</Form.Label>
+                                <Form.Control
+                                    as="textarea"
+                                    placeholder='enter schedule description'
+                                />
+                            </Row>
+                            <Row>
+                                <Form.Label>Choose Date</Form.Label>
+                                <Form.Control
+                                    type="date"
+                                    placeholder='Choose date'
+                                />
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <Form.Label>Start Time</Form.Label>
+                                    <Form.Select>
+                                        <option>Choose Time</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </Form.Select>
+                                </Col>
+                                 <Col>
+                                    <Form.Label>End Time</Form.Label>
+                                    <Form.Select>
+                                        <option>Choose Time</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </Form.Select>
+                                </Col>
+                            </Row>
+                        </Container>
+                        
+                        <div className='create-sched-btn'>
+                            <Button type='submit' onClick={handleCloseModal}>Create Schedule</Button>
+                        </div>
+                    </Form>
+                </Container>
+            </Modal.Body>
+        </Modal>
+    </div>
   )
 }
 
